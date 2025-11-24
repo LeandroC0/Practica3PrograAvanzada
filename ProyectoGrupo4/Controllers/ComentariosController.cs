5a39ebf2bf1cc021e112dd88f6f77793b37391f1
DanielaDiazA <danidiar.14@gmail.com>
2025-11-23 19:24:13 -0600

    blog controller

--- ProyectoGrupo4/Controllers/ComentariosController.cs
+++ ProyectoGrupo4/Controllers/ComentariosController.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using ProyectoGrupo4.Models;
 
 namespace ProyectoGrupo4.Controllers
 {
+    [Authorize]
     public class ComentariosController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
@@ -17,116 +16,47 @@ namespace ProyectoGrupo4.Controllers
         // GET: Comentarios
         public ActionResult Index()
         {
-            var comentarios = db.Comentarios.Include(c => c.User);
-            return View(comentarios.ToList());
-        }
+            var comentarios = db.Comentarios.Include(c => c.User)
+                                            .OrderByDescending(c => c.FechaCreacion)
+                                            .ToList();
 
-        // GET: Comentarios/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Comentario comentario = db.Comentarios.Find(id);
-            if (comentario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(comentario);
+            // Enviar datos requeridos por la práctica
+            ViewBag.Usuario = User.Identity.GetUserName();
+            ViewBag.TotalComentarios = comentarios.Count;
+
+            if (TempData["Mensaje"] != null)
+                ViewBag.Mensaje = TempData["Mensaje"];
+
+            return View(comentarios);
         }
 
         // GET: Comentarios/Create
         public ActionResult Create()
         {
-            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "NombreUsuario");
             return View();
         }
 
         // POST: Comentarios/Create
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
-        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Texto,FechaCreacion,UserId")] Comentario comentario)
+        public ActionResult Create(Comentario comentario)
         {
             if (ModelState.IsValid)
             {
+                comentario.UserId = User.Identity.GetUserId();
+                comentario.FechaCreacion = DateTime.Now;
+
                 db.Comentarios.Add(comentario);
                 db.SaveChanges();
+
+                TempData["Mensaje"] = "Comentario publicado correctamente.";
                 return RedirectToAction("Index");
             }
 
-            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "NombreUsuario", comentario.UserId);
             return View(comentario);
         }
 
-        // GET: Comentarios/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Comentario comentario = db.Comentarios.Find(id);
-            if (comentario == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "NombreUsuario", comentario.UserId);
-            return View(comentario);
-        }
-
-        // POST: Comentarios/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
-        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Texto,FechaCreacion,UserId")] Comentario comentario)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(comentario).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "NombreUsuario", comentario.UserId);
-            return View(comentario);
-        }
-
-        // GET: Comentarios/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Comentario comentario = db.Comentarios.Find(id);
-            if (comentario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(comentario);
-        }
-
-        // POST: Comentarios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Comentario comentario = db.Comentarios.Find(id);
-            db.Comentarios.Remove(comentario);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        // NO necesitas Edit ni Delete para la práctica, pero si los quieres, los mantienes.
+        // En el enunciado, solo se pide ver y crear comentarios.
     }
 }
